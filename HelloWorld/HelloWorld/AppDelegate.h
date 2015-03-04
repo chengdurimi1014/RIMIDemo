18f262dacf308ea1e0fdde929b568a91059ea447
chengdurimi1014 <lanqs@lanqsdeiMac-3.local>
2015-03-04 15:19:29 +0800

    Create a project

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by lanqs on 15/3/4.
+//  Copyright (c) 2015年 rimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by lanqs on 15/3/4.
+//  Copyright (c) 2015年 rimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
